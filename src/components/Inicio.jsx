c8af173c01796ce35303339fbf305d5b038f4aaa
satara2358 <25549072+satara2358@users.noreply.github.com>
2025-03-22 11:49:11 -0500

    Update

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Inicio = () => {
   return (
     <div className="bg-gray-100 p-8 rounded-lg shadow-lg max-w-7xl mx-auto mt-8">
-      <h1 className="text-4xl font-bold text-gray-900 mb-4">Bienvenido a la Página de Inicio</h1>
+      <h1 className="text-4xl font-bold text-gray-900 mb-4">Bienvenido a esta perra pagina</h1>
       <p className="text-lg text-gray-700">
         Esta es la página de inicio donde presentamos información relevante sobre nuestra página.
       </p>
